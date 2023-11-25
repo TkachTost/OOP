c4733ea0a6284e7be60481d0c08143be244f9f3b
Tkach <dmitrijtkac7@gmail.com>
2023-11-26 01:00:00 +0400

    laba-2-5

--- src/test/java/functions/CompositeFunctionTest.java
+++ src/test/java/functions/CompositeFunctionTest.java
@@ -10,10 +10,12 @@ public class CompositeFunctionTest {
     SqrFunction f1=new SqrFunction();
     IdentityFunction f2=new IdentityFunction();
     CompositeFunction tst=new CompositeFunction(f1,f2);
+    CompositeFunction tst2=new CompositeFunction(tst,f1);
     @Test
     public void testApply() {
         Assert.assertEquals(4.0,f2.apply(f1.apply(2)));
         Assert.assertEquals(9.0,f2.apply(f1.apply(3)));
         Assert.assertEquals(16.0,f2.apply(f1.apply(4)));
+        Assert.assertEquals(256.0,tst2.apply(tst.apply(2)));
     }
 }
