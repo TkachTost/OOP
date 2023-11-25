cf848d7592e60f07e702b98696673e0f5e7e2a0f
Tkach <dmitrijtkac7@gmail.com>
2023-11-25 19:24:50 +0400

    laba-2-1

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ru.ssau.tk.laba2.OOP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
