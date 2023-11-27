faf5661f21ffa39ce6b486ba7b6c3e8c48952f77
Tkach <dmitrijtkac7@gmail.com>
2023-11-27 19:21:13 +0400

    laba-2-13

--- src/main/java/functions/MathFunction.java
+++ src/main/java/functions/MathFunction.java
@@ -6,10 +6,4 @@ public interface MathFunction {
       public default CompositeFunction andThen(MathFunction afterFunction) {
           return new CompositeFunction(afterFunction, this);
       }
-//    default MathFunction andThen(MathFunction afterFunction) {
-//        return (double x) -> afterFunction.apply(this.apply(x));
-//    }
-//    public default MathFunction andThen(MathFunction afterFunction) {
-//        return x -> afterFunction.apply(apply(x));
-//    }
 }
